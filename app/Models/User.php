0923e1bd9442a0818194658a358e038d33ab11ce
linuxpedro <pdro.dev97@gmail.com>
2024-10-19 08:27:07 -0300

    feat: add fields and relationships for Plan and User models

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Model
 {
+    use HasFactory, Notifiable;
+
     protected $hidden = ['created_at', 'updated_at'];
+
+    protected $fillable = [
+        'name',
+        'email',
+    ];
+
+    public function contracts()
+    {
+        return $this->hasMany(Contract::class);
+    }
+
+    public function activeContract()
+    {
+        return $this->hasOne(Contract::class)->where('active', true);
+    }
+
+    public function activePlan()
+    {
+        return $this->hasOneThrough(Plan::class, Contract::class)
+            ->where('contracts.active', true);
+    }
 }
